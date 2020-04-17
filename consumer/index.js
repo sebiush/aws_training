23fee81e1f2532b9a83791afab8d0fa81080f4fe
Petla <212626796@logon.ds.ge.com>
2020-04-17 20:12:17 +0200

    Moved services host addres to env variable configuration.

--- consumer/index.js
+++ consumer/index.js
@@ -3,16 +3,18 @@ const app = express();
 const axios = require('axios');
 
 const PORT = process.env.PORT || 3002;
+const ACCESS_ALLOW_ORIGIN = process.env.ACCESS_ALLOW_ORIGIN || 'http://localhost:3000';
+const STOCK_HOST = 'http://localhost:3003';
 
 const sendResponse = (res, data) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Origin', ACCESS_ALLOW_ORIGIN);
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.send(data);
 };
 
 app.post('/api/consume', (req, res, next) => {
   console.log('consumed');
-  axios.post('http://localhost:3003/api/consume')
+  axios.post(`${STOCK_HOST}/api/consume`)
     .then(() => { sendResponse(res, 'consumed') })
     .catch(error => { sendResponse(res, error) });
 });
--- producer/index.js
+++ producer/index.js
@@ -3,16 +3,18 @@ const app = express();
 const axios = require('axios');
 
 const PORT = process.env.PORT || 3001;
+const ACCESS_ALLOW_ORIGIN = process.env.ACCESS_ALLOW_ORIGIN || 'http://localhost:3000';
+const STOCK_HOST = 'http://localhost:3003';
 
 const sendResponse = (res, data) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Origin', ACCESS_ALLOW_ORIGIN);
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.send(data);
 };
 
 app.post('/api/produce', (req, res, next) => {
   console.log('produced');
-  axios.post('http://localhost:3003/api/produce')
+  axios.post(`${STOCK_HOST}/api/produce`)
     .then(() => { sendResponse(res, 'produced') })
     .catch(error => { sendResponse(res, error) });
 });
